e8ac2166ef22d87ef584d07c17b8f8e392e8e088
jeknotts <jacobknotts@hotmail.com>
2016-01-28 08:44:08 -0500

    Update chip.sh
    The change is to allow someone to flash the chip operating system from a lenux bootable usb, without the need to install linux.

--- chip.sh
+++ chip.sh
@@ -17,12 +17,12 @@ echo "Menu..."
 echo "======================================="
 echo ""
 echo "Enter 1 to Setup Ubuntu For Flashing 1:"
-echo ""
+echo "Enter 2 to Setup Ubuntu on USB For Flashing 2:"
 echo "Flash-Tool"
-echo "Enter 2 to Flash With NTC Buildroot OS 2:"
-echo "Enter 3 to Flash With Debian 3:"
-echo "Enter 4 to Flash With CHIP Operating System 4:"
-echo "Enter 5 to exit"
+echo "Enter 3 to Flash With NTC Buildroot OS 3:"
+echo "Enter 4 to Flash With Debian 4:"
+echo "Enter 5 to Flash With CHIP Operating System 5:"
+echo "Enter 6 to exit"
 echo ""
 echo "======================================="
 echo "Enter option Number:"
@@ -42,18 +42,32 @@ git clone http://github.com/NextThingCo/sunxi-tools
 		cd .. 
 		git clone http://github.com/NextThingCo/CHIP-tools 
 		cd CHIP-tools;;
+		
+2)     sudo add-apt-repository universe
+       sudo apt-get update
+       sudo apt-get install u-boot-tools android-tools-fastboot git build-essential libusb-1.0-0-dev libncurses5-dev libc6-i386 lib32stdc++6 lib32z1 android-tools-fsutils
+   mkdir chip-files
+   cd chip-files
+git clone http://github.com/NextThingCo/sunxi-tools
+		cd sunxi-tools
+		make
+	sudo rm -f /usr/local/bin/fel
+	sudo ln -s $PWD/fel /usr/local/bin/fel
+		cd .. 
+		git clone http://github.com/NextThingCo/CHIP-tools 
+		cd CHIP-tools;;
 
 
-2) echo "Prepare CHIP with a jumper wire connecting Pin 7 and Pin 39 on header U14 (UBOOT pin and GND)."
-  cd chip-files/CHIP-tools
-  sudo ./chip-update-firmware.sh;;
 3) echo "Prepare CHIP with a jumper wire connecting Pin 7 and Pin 39 on header U14 (UBOOT pin and GND)."
   cd chip-files/CHIP-tools
+  sudo ./chip-update-firmware.sh;;
+4) echo "Prepare CHIP with a jumper wire connecting Pin 7 and Pin 39 on header U14 (UBOOT pin and GND)."
+  cd chip-files/CHIP-tools
   sudo ./chip-update-firmware.sh -d ;;
-4)echo "Prepare CHIP with a jumper wire connecting Pin 7 and Pin 39 on header U14 (UBOOT pin and GND)."
+5)echo "Prepare CHIP with a jumper wire connecting Pin 7 and Pin 39 on header U14 (UBOOT pin and GND)."
   cd chip-files/CHIP-tools
   sudo ./chip-update-firmware.sh -d -b stable-gui ;;  
-5) exit;;
+6) exit;;
 esac
 echo -e "Enter new option"
 done
